c301677a1ae27011fb3a95a8bbc2e261d117b0f2
SayanthD <sayanthd@gmail.com>
2020-09-26 19:32:40 +0530

    aria2: fix file paths

--- torrent-leecher.sh
+++ torrent-leecher.sh
@@ -7,5 +7,5 @@ aria2c \
 	--rpc-listen-port=6800 \
 	--rpc-max-request-size=1024M \
 	--bt-stop-timeout=600 \
-	--conf-path=/app/tobrot/aria2/aria2.config &
+	--conf-path=/app/tobrot/aria2/aria2.conf &
 python3 -m tobrot
