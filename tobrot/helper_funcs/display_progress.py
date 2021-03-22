4fb2c136f4a7022de392461961d1200bdc20412f
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-03-22 22:33:50 +0530

    display_progress.py: Misc changes

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -2,81 +2,69 @@
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
+import asyncio
 import math
 import time
+
+from pyrogram.errors import FloodWait
 
 from tobrot.config import Config
 
 
-async def progress_for_pyrogram(current, total, ud_type, message, start):
+async def progress_for_pyrogram(current, total, status_text, message, start):
     now = time.time()
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
-        # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
         elapsed_time = round(diff)
         if elapsed_time == 0:
             return
         speed = current / elapsed_time
         time_to_completion = round((total - current) / speed)
-        # estimated_total_time = elapsed_time + time_to_completion
-
-        # elapsed_time = time_formatter(elapsed_time)
         estimated_total_time = time_formatter(time_to_completion)
 
-        progress = "[{}{}] \nProgress: {}%".format(
+        progress_block = "[{}{}]\n".format(
             "".join(
-                [
-                    Config.FINISHED_PROGRESS_STR
-                    for _ in range(math.floor(percentage / 5))
-                ]
+                Config.FINISHED_PROGRESS_STR
+                for _ in range(math.floor(percentage / 5))
             ),
             "".join(
-                [
-                    Config.UN_FINISHED_PROGRESS_STR
-                    for _ in range(20 - math.floor(percentage / 5))
-                ]
+                Config.UN_FINISHED_PROGRESS_STR
+                for _ in range(20 - math.floor(percentage / 5))
             ),
-            round(percentage, 2),
         )
 
-        tmp = progress + " of {} at {}/s, ETA: {}\n".format(
-            # humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),
-            estimated_total_time if estimated_total_time else "0 seconds",
+        progress_text = (
+            f"{progress_block}"
+            f"Uploading {round(percentage, 2)}% of "
+            f"{humanbytes(total)} @ {humanbytes(speed)}/s, "
+            f"ETA: {estimated_total_time}\n"
         )
         try:
             if not message.photo:
-                await message.edit_text(text="{}\n {}".format(ud_type, tmp))
+                await message.edit_text(text=f"{status_text}\n {progress_text}")
             else:
-                await message.edit_caption(caption="{}\n {}".format(ud_type, tmp))
-        except:
-            pass
+                await message.edit_caption(caption=f"{status_text}\n {progress_text}")
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
 
 
 def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2 ** 10
-    n = 0
-    Dic_powerN = {0: " ", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + "B"
+    # https://stackoverflow.com/a/43690506
+    for unit in ["B", "KiB", "MiB", "GiB", "TiB", "PiB"]:
+        if size < 1024.0 or unit == "PiB":
+            break
+        size /= 1024.0
+    return f"{size:.2f} {unit}"
 
 
 def time_formatter(seconds: int) -> str:
     result = ""
-    v_m = 0
     remainder = seconds
     r_ange_s = {"days": (24 * 60 * 60), "hours": (60 * 60), "minutes": 60, "seconds": 1}
     for age, divisor in r_ange_s.items():
         v_m, remainder = divmod(remainder, divisor)
         v_m = int(v_m)
         if v_m != 0:
-            result += f" {v_m} {age} "
-    return result
+            result += f"{v_m} {age} "
+    return result or "0 seconds"
